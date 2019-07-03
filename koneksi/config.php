bb6bd4d9cfde98223aad360938673b1b2ab69e99
Khoirul02 <hudak006@gmail.com>
2019-07-03 09:05:55 +0700

    user saja

---
+++ koneksi/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = "localhost";
+$user = "app_iav";
+$password = "kwh2xxbtE0";
+$nama_database = "app_iav";
+
+$db = mysqli_connect($server, $user, $password, $nama_database);
+
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+
+?>
